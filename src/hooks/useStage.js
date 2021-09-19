9c583c8f2f1ac28b4f68b037ceef856b50ca1aeb
Nick Strong <nstrong92@yahoo.com>
2021-09-19 12:06:24 -0600

    added functionality based on player position in the stage useStage.js

--- src/hooks/useStage.js
+++ src/hooks/useStage.js
@@ -1,10 +1,40 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 import { createStage } from "../gameHelpers";
 
-export const useStage = () => {
+export const useStage = (player, resetPlayer) => {
   // will generate initial stage
   const [stage, setStage] = useState(createStage());
+
+  useEffect(() => {
+    const updateStage = (prevStage) => {
+      // first have to clear stage from previous render
+      const newStage = prevStage.map((row) =>
+        row.map((cell) => (cell[1] === "clear" ? [0, "clear"] : cell))
+      );
+
+      // draw shape and stage
+      // looping through shapes to see what cell the shape is occupying and know what shape it is
+
+      player.tetromino.forEach((row, y) => {
+        row.forEach((value, x) => {
+          // if value is not 0 then i know that makes up shape and how i can position on the stage
+          if (value !== 0) {
+            // coordinates of shape on the stage
+            newStage[y + player.pos.y][x + player.pos.x] = [
+              value,
+              // calculating what to render based on merged(collided) or clear(flush stage)
+              `${player.collided ? "merged" : "clear"}`,
+            ];
+          }
+        });
+      });
+
+      return newStage;
+    };
+
+    setStage((prev) => updateStage(prev));
+  }, [player.collided, player.pos.x, player.pos.y, player.tetromino]);
 
   return [stage, setStage];
 };
